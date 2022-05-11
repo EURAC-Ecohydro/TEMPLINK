239602fa8b5fad2e5ed4b1fabd793979af6195d7
yakopuz <giacomo.bertoldi@eurac.edu>
2022-05-11 12:44:21 +0200

    Added header

--- R_Scripts/Sim_vs_Obs3_1.r
+++ R_Scripts/Sim_vs_Obs3_1.r
@@ -1,3 +1,26 @@
+#!/usr/bin/env Rscript
+# file Sim_vs_Obs3_1.r
+#
+# This script is a first example for comparing GEOtop outputs 
+# with Templink simuations
+#
+# author: Raul Serban, Giacomo Bertoldi on 11-05-2022
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+###############################################################################
+
 
 #### Compare soil temperature simulation to observation at station B2 ####
 library(geotopbricks)
